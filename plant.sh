742a7ab638cff8bd1da8496f40ac76f18725bf44
June Lattanzi <jlattanzi@signifyhealth.com>
2025-12-24 09:59:12 -0800

    playing with gum

---
+++ plant.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob
+
+function make_backup() {
+  mv "$1" "$1.bak"
+}
+
+function delete_existing() {
+  echo "would you like to delete the existing configuration?"
+  gum confirm && rm -rf "$1" || (echo "okay then!" && exit 0)
+}
+
+valid_figs=()
+exclude=("scripts" ".git")
+configurations=(*/)
+
+# get the valid configs
+for fig in "${configurations[@]}"; do
+  fig="${fig%/}"
+  if [[ ! " ${exclude[*]} " =~ ${fig} ]]; then
+    valid_figs+=("$fig")
+  fi
+done
+
+# choose wisely
+choice=$(gum filter --height 10 ${valid_figs[*]})
+echo "$choice"
+
+# check if existing config is present
+destination_path="$HOME/.config/$choice"
+if [[ -d "$destination_path" ]]; then
+  echo "configuration for $choice already exists!"
+  echo "would you like to create a backup? (${choice}.bak)"
+  gum confirm && make_backup "$destination_path" || delete_existing "$destination_path"
+fi
+
+# create the symlink
+ln -s "$PWD/$choice" "$destination_path"
--- scripts/initial.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set +e
-
-sudo pacman -S \
-  base-devel gcc clang tmux yazi rustup firefox discord \
-  zsh kitty
