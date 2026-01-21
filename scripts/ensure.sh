1e861fc3961f6d117465465da6c077f77737af01
June <git@jml.sh>
2026-01-21 09:34:43 -0800

    bruh

---
+++ scripts/ensure.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ENSURE_INSTALLED=(zsh yazi tmux gcc clang cmake make libtool meson cpio git waybar hyprshot rofi dunst)
--- scripts/zsh-plugins.sh
+++ scripts/zsh-plugins.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-[[ -d ~/.config/oh-my-zsh ]] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
